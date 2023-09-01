042d0bff04c694798c8aa965bdd15d9521f2e3c4
morshed61 <morshedsarkar15219@gmail.com>
2023-09-01 23:15:05 +0600

    Added function

--- currencyconverterfinal.c
+++ currencyconverterfinal.c
@@ -1,42 +1,28 @@
 #include<stdio.h>
-int main()
+char currencyname[][10]= {"BDT","USD","AUD","INR","GBP","SGD","JPY","RUB","EUR","CAD"};
+float usdtoanylist[]= {109.86,1,1.56,82.64,0.80,1.36,146.06,94.80,0.93,1.36};
+float anytousdlist[]= {0.0091,1,0.64,0.012,1.26,0.74,0.0068,0.011,1.08,0.74};
+
+void functoconv(int from,int to,int value)
 {
-    while(1)
-    {
-    int exit;
-    char currencyname[][10]={"BDT","USD","AUD","INR","GBP","SGD","JPY","RUB","EUR","CAD"};
-    float usdtoanylist[]={109.86,1,1.56,82.64,0.80,1.36,146.06,94.80,0.93,1.36};
-    float anytousdlist[]={0.0091,1,0.64,0.012,1.26,0.74,0.0068,0.011,1.08,0.74};
-    int from=0,to=0,value=0;
-    printf("Options: 1.BDT 2.USD 3.AUD 4.INR 5.GBP 6.SGD 7.JPY 8.RUB 9.EUR 10.CAD\n");
-    printf("Currency to convert from==> ");
-    scanf("%d",&from);
-    printf("Currency to convert to==> ");
-    scanf("%d",&to);
-    printf("\n%s to %s\n\n",currencyname[from-1],currencyname[to-1]);
-    printf("Amount to convert==> ");
-    scanf("%d",&value);
-
-    printf("\n\n");
-
     if(from!=to)
     {
-    if(from==2)//usd to any
-        {
-           float calctusd = usdtoanylist[to-1]*value;
-           printf("%d %s to %s is => %.2f %s\n\n",value,currencyname[from-1],currencyname[to-1],calctusd,currencyname[to-1]);
-        }
 
-        else if(to==2)//any to usd
+        if(to==2)//any to usd
         {
             float calctany = anytousdlist[from-1]*value;
-            printf("%d %s to %s is => %.2f %s\n\n",value,currencyname[from-1],currencyname[to-1],calctany,currencyname[to-1]);
+            printf("\n%d %s to %s is => %.2f %s\n\n",value,currencyname[from-1],currencyname[to-1],calctany,currencyname[to-1]);
         }
-        else//any to any
+        else if(from==2)//usd to any
+        {
+            float calctusd = usdtoanylist[to-1]*value;
+            printf("\n%d %s to %s is => %.2f %s\n\n",value,currencyname[from-1],currencyname[to-1],calctusd,currencyname[to-1]);
+        }
+        else
         {
             float anytousd = anytousdlist[from-1]*value;
             float calculated = usdtoanylist[to-1]*anytousd;
-            printf("%d %s to %s is => %.2f %s\n\n",value,currencyname[from-1],currencyname[to-1],calculated,currencyname[to-1]);
+            printf("\n%d %s to %s is => %.2f %s\n\n",value,currencyname[from-1],currencyname[to-1],calculated,currencyname[to-1]);
         }
     }
     else if(from==to)
@@ -44,6 +30,26 @@ int main()
         float values=(float)value;
         printf("%d %s to %s is => %.2f %s\n\n",value,currencyname[from-1],currencyname[to-1],values,currencyname[to-1]);
     }
+
+
+
+}
+int main()
+{
+    while(1)
+    {
+        int exit;
+        int from=0,to=0,value=0;
+        printf("Options: 1.BDT 2.USD 3.AUD 4.INR 5.GBP 6.SGD 7.JPY 8.RUB 9.EUR 10.CAD\n");
+        printf("Currency to convert from==> ");
+        scanf("%d",&from);
+        printf("Currency to convert to==> ");
+        scanf("%d",&to);
+        printf("\n%s to %s\n\n",currencyname[from-1],currencyname[to-1]);
+        printf("Amount to convert==> ");
+        scanf("%d",&value);
+        functoconv(from,to,value);
+        printf("\n\n");
         printf("Exit?\n\n1.Yes 2.No\n\n");
         scanf("%d",&exit);
         switch(exit)
@@ -54,6 +60,5 @@ int main()
         case 2:
             continue;
         }
-
     }
 }
